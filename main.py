a1ce22539e515069a27a6065474a660f5691ff89
Bezhenar Denis <bezhenardv@gmail.com>
2025-07-23 01:59:05 +0300

    убрал все комменты в коде

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 
-def num(a: list):  # Подсказка: a должен быть списком
+def num(a: list):
     return len(a)
 
-# Вызовы функции
-print(num([1, 2, 3]))  # OK (тип list)
-print(num("abc"))       # Ошибки не будет, но тип не list!
+
+print(num([1, 2, 3]))
+print(num("abc"))
